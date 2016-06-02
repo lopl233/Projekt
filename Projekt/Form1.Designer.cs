1d27633d1c468301e45b6e15bdbf4195f16f4a18
lopl233 <lopl233@o2.pl>
2016-06-02 11:43:36 +0200

    Kreator uzytkownika

--- Projekt/Form1.Designer.cs
+++ Projekt/Form1.Designer.cs
@@ -33,11 +33,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 81);
+            this.textBox1.Location = new System.Drawing.Point(93, 52);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(137, 20);
             this.textBox1.TabIndex = 0;
@@ -45,7 +47,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 84);
+            this.label1.Location = new System.Drawing.Point(40, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 2;
@@ -55,7 +57,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 125);
+            this.label2.Location = new System.Drawing.Point(40, 101);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 3;
@@ -63,9 +65,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(106, 163);
+            this.button1.Location = new System.Drawing.Point(93, 133);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(137, 33);
             this.button1.TabIndex = 4;
             this.button1.Text = "Zaloguj";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,17 +75,38 @@
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(93, 118);
+            this.maskedTextBox1.Location = new System.Drawing.Point(93, 94);
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.PasswordChar = '*';
             this.maskedTextBox1.Size = new System.Drawing.Size(137, 20);
             this.maskedTextBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stworz konto";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nie masz jeszcze konta?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -104,6 +127,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
---
+++ Projekt/Form4.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Projekt
+{
+    public partial class Form4 : Form
+    {
+        string connstr;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        public Form4(string connstr)
+        {
+            this.connstr=connstr;
+
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(connstr);
+            try
+            { conn.Open(); }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Nie można połączyć się z serwerem");
+            }
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || maskedTextBox1.Text == "")
+            {
+                MessageBox.Show("Uzupełnij wszystkie pola");
+                conn.Close();
+                return;
+            }
+
+            string sql ="INSERT INTO 'klienci' ('ID_KLIENTA' ,'IMIE' ,'NAZWISKO' ,'ADRES')VALUES((SELECT MAX(ID_KLIENTA)+1 FROM klienci),'"
+                + textBox1.Text + "','"
+                + textBox2.Text + "','"
+                + textBox3.Text + "')";
+
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) { ex.ToString(); conn.Close(); }
+            conn.Close();
+        }
+    }
+}
